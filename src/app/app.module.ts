75def2f4c1fff4aa7335a14c03ff83f0caf98765
Nidhi <54665514+NidhiR88@users.noreply.github.com>
2020-07-29 11:50:45 +0530

    current-time-component (#5)
    * current-time-component
    
    * created button component
    
    * changes in component structure
    
    * changes in container alignments
    
    * remove spacing
    
    * remove space

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DigitalClockComponent } from './digital-clock/digital-clock.component';
 import 'boxicons';
-
-
+import { CurrentTimeComponent } from './digital-clock/current-time/current-time.component';
+import { ButtonControlComponent } from './digital-clock/set-time/button-control/button-control.component';
+import { PlusMinusButtonComponent } from './digital-clock/set-time/plus-minus-button/plus-minus-button.component';
+import { SetTimeComponent } from './digital-clock/set-time/set-time.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DigitalClockComponent
+    DigitalClockComponent,
+    CurrentTimeComponent,
+    ButtonControlComponent,
+    PlusMinusButtonComponent,
+    SetTimeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
    
   ],
   providers: [],
---
+++ src/app/digital-clock/set-time/button-control/button-control.component.ts
@@ -0,0 +1,68 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button-control',
+  templateUrl: './button-control.component.html',
+  styleUrls: ['./button-control.component.scss']
+})
+export class ButtonControlComponent {
+  @Input() alarmHours:number;
+  @Input() alarmMinutes:number;
+  alarmActive = false;
+  IsAlarmSet:boolean = true;
+  checkAlarmInterval: any;
+  soundPlaying:boolean = false;
+  audio = new Audio('./assets/Alarm-ringtone1.mp3');
+  
+  setAlarm() {
+    this.IsAlarmSet = false;
+    this.checkAlarmInterval = setInterval(()=> {
+    this.alarmInterval() }, 1000);
+    
+  }
+
+  startAudio() {
+    this.alarmActive = true;
+
+    if(!this.soundPlaying){
+      this.audio.load();
+      this.audio.play();
+      this.soundPlaying = true;
+    }
+
+  }
+
+  stopAudio() {
+    this.alarmActive = false;
+
+    if(this.soundPlaying){
+      this.audio.pause();
+      this.audio.currentTime = 0;
+      this.soundPlaying = false;
+    }
+  }
+
+  alarmInterval() {
+    debugger
+    var checkTime = new Date();
+	  var hours = checkTime.getHours();
+    var minutes = checkTime.getMinutes();
+    
+    if ((hours == this.alarmHours) && (minutes == this.alarmMinutes )){
+
+      this.IsAlarmSet = true;
+      this.startAudio();
+      this.alarmActive = true;
+      
+      clearInterval(this.checkAlarmInterval);
+    }
+  }
+
+  setAlarmStatus() {
+    this.stopAudio();
+    this.IsAlarmSet = true;
+    this.alarmActive = false;
+    
+  }
+
+}
